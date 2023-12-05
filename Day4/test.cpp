603a7d4ba1ae5ef8fe6412fc99dd899646c69049
glen <glenwojnar@yahoo.com>
2023-12-04 20:42:08 -0700

    finished day 4

--- Day4/test.cpp
+++ Day4/test.cpp
@@ -1,109 +1,164 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 #include <sstream>
-#include <set>
-#include <map>
-#include <queue>
+#include <cstdio>
+#include <algorithm>
+
+std::vector<std::string> processInput()
+{
+    std::string s;
+    std::vector<std::string> input;
+    while(std::getline(std::cin, s))
+    {
+        input.push_back(s);
+    }
+    return input;
+}
+
+//https://stackoverflow.com/a/14193797
+template<typename T>
+static constexpr inline T pown(T x, unsigned p)
+{
+    T result = 1;
+
+    while (p)
+    {
+        if (p & 0x1)
+        {
+            result *= x;
+        }
+        x *= x;
+        p >>= 1;
+    }
+
+    return result;
+}
+
+long long part1(const std::vector<std::string>& input)
+{
+    long long sum = 0;
+    for(auto& card : input)
+    {
+        std::stringstream ss (card);
+
+        std::string command;
+
+        std::vector<long long> winning_number;
+
+        long long i = 0;
+
+        bool is_winning_card = false;
+        bool is_input_number_you_have = false;
+
+        long long number_of_win = 0;
+
+        while(ss >> command)
+        {
+            ++i;
+
+            if(i <= 2) continue;
+
+            if(command == "|") is_input_number_you_have = true;
+
+            else if(is_input_number_you_have)
+            {
+                int number_you_have = std::stoi(command);
+                if(std::find(winning_number.begin(), winning_number.end(), number_you_have) != winning_number.end())
+                {
+                    is_winning_card = true;
+                    ++number_of_win;
+                }
+            }
+            else
+            {
+                winning_number.push_back(std::stoi(command));
+            }
+        }
+        if(is_winning_card)
+        {
+            sum += pown(2, number_of_win - 1);
+        }
+    }
+    return sum;
+}
+int scratchcards_score(const std::vector<std::string>& input,  std::vector<int>& dp, int card_number)
+{
+    //std::cout << card_number << '\n';
+    std::stringstream ss (input[card_number]);
+
+    std::string command;
+
+    std::vector<long long> winning_number;
+
+    long long i = 0;
+
+    bool is_winning_card = false;
+    bool is_input_number_you_have = false;
+
+    long long number_of_win = 0;
+    while(ss >> command)
+    {
+        ++i;
+
+        if(i <= 2) continue;
+
+        if(command == "|") is_input_number_you_have = true;
+
+        else if(is_input_number_you_have)
+        {
+            int number_you_have = std::stoi(command);
+            if(std::find(winning_number.begin(), winning_number.end(), number_you_have) != winning_number.end())
+            {
+                is_winning_card = true;
+                ++number_of_win;
+            }
+        }
+        else
+        {
+            winning_number.push_back(std::stoi(command));
+        }
+    }
+    if(is_winning_card)
+    {
+        if(dp[card_number] == -1)
+        {
+            int sum = 0;
+            for(int j = 1; j <= number_of_win; ++j)
+            {
+                sum += scratchcards_score(input, dp, card_number + j);
+            }
+            sum += 1;
+            dp[card_number] = sum;
+        }
+    }
+    else
+    {
+        dp[card_number] = 1;
+    }
+    return dp[card_number];
+}
+
+long long part2(const std::vector<std::string>& input)
+{
+    std::vector<int> dp(input.size());
+    std::fill(dp.begin(), dp.end(), -1);
+    long long sum = 0;
+    for(int i = 0; i < input.size(); ++i)
+    {
+        sum += scratchcards_score(input, dp,  i);
+    }
+    return sum;
+}
+
 
 int main()
 {
-    std::ifstream inputFile("input.txt");
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Error opening the input file." << std::endl;
-        return 1;
-    }
+    std::freopen("input.txt", "r", stdin);
+    //std::freopen("day4out.txt", "w", stdout);
 
-    std::string line;
-    int score = 0;
+    std::vector<std::string> input = processInput();
 
-    // Using a map to represent the scratchcards and their counts
-    std::map<int, int> scratchcardsCount;
-
-    while (std::getline(inputFile, line))
-    {
-        std::vector<std::string> tokens;
-        size_t pos = 0;
-        std::string token;
-        int matches = 0;
-
-        while ((pos = line.find(":")) != std::string::npos)
-        {
-            token = line.substr(pos + 1);
-            tokens.push_back(token);
-            line.erase(0, pos + 1);
-        }
-
-        // Using a queue to keep track of cards to be processed
-        std::queue<int> cardsToProcess;
-
-        for (auto &token : tokens)
-        {
-            std::istringstream iss(token);
-            std::vector<std::string> gameNumsArray;
-            std::vector<std::string> winningNumsArray;
-
-            // Read until '|'
-            while (iss >> token && token != "|")
-            {
-                winningNumsArray.push_back(token);
-            }
-
-            // Read remaining tokens
-            while (iss >> token)
-            {
-                gameNumsArray.push_back(token);
-            }
-
-            std::set<std::string> winningNumsSet(winningNumsArray.begin(), winningNumsArray.end());
-
-            for (auto &num : gameNumsArray)
-            {
-                auto it = winningNumsSet.find(num);
-                if (it != winningNumsSet.end())
-                {
-                    matches++;
-                }
-            }
-
-            // Processing winnings for each scratchcard
-            if (matches > 0)
-            {
-                for (int i = 1; i <= matches; i++)
-                {
-                    // Increase the count for the next scratchcard
-                    scratchcardsCount[i]++;
-                    // Add the next scratchcard to the queue for processing
-                    cardsToProcess.push(i);
-                }
-            }
-        }
-
-        // Process the queue to account for overlaps
-        while (!cardsToProcess.empty())
-        {
-            int currentCard = cardsToProcess.front();
-            cardsToProcess.pop();
-
-            // Process winnings for the current card
-            for (int i = currentCard + 1; i <= currentCard + matches; i++)
-            {
-                scratchcardsCount[i]++;
-                cardsToProcess.push(i);
-            }
-        }
-    }
-
-    // Print the final count of scratchcards
-    for (const auto &entry : scratchcardsCount)
-    {
-        std::cout << "Number of cards with " << entry.first << " matches: " << entry.second << std::endl;
-        score += entry.second;
-    }
-
-    std::cout << "Total Score: " << score << " scratchcards." << std::endl;
-    inputFile.close();
-    return 0;
+    std::cout << "The solution for part 1 is: " << part1(input) <<'\n';
+    std::cout << "The solution for part 2 is: " << part2(input) <<'\n';
 }
